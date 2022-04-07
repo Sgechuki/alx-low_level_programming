a6d46665e18767814aafa91728243e1a25d8fcaa
Sgechuki <sgechuki@outlook.com>
2022-04-08 00:19:13 +0300

    Use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int snt;
+	snt = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s\n",snt);
+	return (0);
+}
