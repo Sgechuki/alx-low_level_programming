86f6fabfaa514f412897a082fcc3988b6a13f4de
Sgechuki <sgechuki@outlook.com>
2022-04-08 00:00:08 +0300

    Use puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio>
+#include <stdio.h>
 int main(void)
 {
-	char snt = "Programming is like building a multilingual puzzle";
+	char snt = "\"Programming is like building a multilingual puzzle";
 	puts(snt);
 	return (0)
 }
