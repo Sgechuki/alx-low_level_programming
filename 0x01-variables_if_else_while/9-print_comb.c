9e738c3490b7c76914531dff06b3999f4c023444
Sgechuki <sgechuki@outlook.com>
2022-04-09 09:41:58 +0300

    Print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,19 @@
 int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
+		if (i != 9)
+		{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
+		}
+		else
+		{
+			putchar(i + '0');
+		}
 	}
-	putchar('$');
 	return (0);
 }
