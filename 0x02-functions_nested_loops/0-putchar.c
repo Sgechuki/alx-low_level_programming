0cb2bfcbd0daba7feafd5b675d6de3f8fe25642f
Sgechuki <sgechuki@outlook.com>
2022-04-12 21:53:51 +0300

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ * Description - Program that prints "_putchar"
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(1, "_putchar\n", 8);
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory holds my submisions to ALX C - Functions, nested loops
