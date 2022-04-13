4d8ce9e00232826ac6b44a36a1e2cef36027b76f
Sgechuki <sgechuki@outlook.com>
2022-04-13 03:02:19 +0300

    Compute abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * abs - computes absolute value of a number
+ * _abs - computes absolute value of a number
  * @n: - is the character to be checked
  * Return: Always 0
  */
 int _abs(int n)
 {
 int result;
-if ( n >= 0)
+if (n >= 0)
 {
 	result = n;
 }
