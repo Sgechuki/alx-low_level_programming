6957f93d4af0c26cac97c080e542e31213b51e79
Sgechuki <sgechuki@outlook.com>
2022-04-15 15:30:53 +0300

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,22 +14,18 @@ for (i = 1; i <= 100; i++)
 	if ((i % 3) == 0)
 	{
 	printf("Fizz");
-	printf(" ");
 	}
 	else if ((i % 5) == 0)
 	{
 	printf("Buzz");
-	printf(" ");
 	}
 	else if ((i % 3) == 0 && (i % 5) == 0)
 	{
 	printf("FizzBuzz");
-	printf(" ")
 	}
 	else
 	{
 	printf("%d", i);
-	printf(" ")
 	}
 }
 printf("\n");
