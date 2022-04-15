4b7d062e40796a86f59d44ab2d15e058b8c94ed0
Sgechuki <sgechuki@outlook.com>
2022-04-15 07:41:42 +0300

    Checks uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,17 +4,15 @@
  * @c - is the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
+
 int _isupper(int c)
 {
-int result;
-
 if (c >= 'A' && c <= 'Z')
 {
-	result = 1;
+return (1);
 }
 else
 {
-	result = 0;
+return (0);
 }
-return (result);
 }
