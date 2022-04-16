9d8a788435aff5cc54e8fa85b56e435c75cac0f6
Sgechuki <sgechuki@outlook.com>
2022-04-16 21:51:08 +0300

    Adhere to betty style

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,35 +9,35 @@ int i, x;
 
 for (i = 0; i <= 9; i++)
 {
-	for (x = 0; x <= 9; x++)
-	{
-	int result;
+for (x = 0; x <= 9; x++)
+{
+int result;
 
-	result = (i * x);
-		if (result <= 9 && x != 0) 
-		{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(result + '0');
-		}
-		else if (result <= 9 && x == 0)
-		{
-		_putchar(result + '0');
-		}
-		else if (result >= 10 && x != 0)
-		{
-		_putchar(',');
-                _putchar(' ');
-		_putchar((result / 10) + '0');
-		_putchar((result % 10) + '0');
-		}
-		else if (result >= 10 && x == 0)
-		{
-		_putchar((result / 10) + '0');
-                _putchar((result % 10) + '0');
-		}
-	}
+result = (i * x);
+if (result <= 9 && x != 0) 
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(result + '0');
+}
+else if (result <= 9 && x == 0)
+{
+_putchar(result + '0');
+}
+else if (result >= 10 && x != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+}
+else if (result >= 10 && x == 0)
+{
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+}
+}
 _putchar('\n');
 }
 }
