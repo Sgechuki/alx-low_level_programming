0960947b798e96d010dc1acf5ab75720bcfc9c36
Sgechuki <sgechuki@outlook.com>
2022-04-16 21:36:35 +0300

    Fix spacing bug on output

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,32 +13,31 @@ for (i = 0; i <= 9; i++)
 	{
 	int result;
 
-	result = ( i * x );
-		if (result <= 9 && x != 9) 
+	result = (i * x);
+		if (result <= 9 && x != 0) 
 		{
-		_putchar(result + '0');
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
+		_putchar(result + '0');
 		}
-		else if (result <= 9 && x == 9)
+		else if (result <= 9 && x == 0)
 		{
 		_putchar(result + '0');
-		_putchar('\n');
 		}
-		else if (result >= 10 && x != 9)
+		else if (result >= 10 && x != 0)
 		{
+		_putchar(',');
+                _putchar(' ');
 		_putchar((result / 10) + '0');
 		_putchar((result % 10) + '0');
-		_putchar(',');
-		_putchar(' ');
 		}
-		else if (result >= 10 && x == 9)
+		else if (result >= 10 && x == 0)
 		{
 		_putchar((result / 10) + '0');
                 _putchar((result % 10) + '0');
-		_putchar('\n');
 		}
 	}
+_putchar('\n');
 }
 }
