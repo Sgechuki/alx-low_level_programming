1787a69abfe12c8b3c32ab8359b87db5ccdf9a09
Sgechuki <sgechuki@outlook.com>
2022-04-17 12:35:30 +0300

    Print combinations of two two digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ for (k = '0'; k <= '9'; k++)
 {
 for (l = '0'; l <= '9'; l++)
 {
-if (i <= k && j <= l)
+if (k == 0 && l == 0)
 {
 	putchar(i);
 	putchar(j);
@@ -25,7 +25,7 @@ if (i <= k && j <= l)
 	putchar(k);
 	putchar(l);
 }
-if (i <= k && j <= l && i != '9' && j != '8')
+if (k == '0' && l == '0' && l != '9' && j != '8')
 {
 	putchar(',');
 	putchar(' ');
