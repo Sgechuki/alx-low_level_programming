ddedc8f906f119830bacc4b9f2aa1e75f51ad9cb
Sgechuki <sgechuki@outlook.com>
2022-04-17 09:42:16 +0300

    Deal with delimiters

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,20 +9,20 @@ int main(void)
 {
 	int i, n;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '8'; i++)
 	{
 		for (n = '1'; n <= '9'; n++)
 		{
-			if ((i != n && i < n) && (i != '0' && n != '1'))
-			{
-				putchar(',');
-				putchar(' ');
-			}
 			if (i != n && i < n)
 			{
 				putchar(i);
 				putchar(n);
 			}
+			if (i != n && i < n && n != '8')
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
 		}
 	}
 putchar('\n');
