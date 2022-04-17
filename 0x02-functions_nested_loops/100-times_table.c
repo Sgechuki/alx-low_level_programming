5eecc67d2e6d6fc67ad03a4d1f99fe729f7c752a
Sgechuki <sgechuki@outlook.com>
2022-04-17 14:19:21 +0300

    Print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
  */
 void print_times_table(int n)
 {
-if (n <= 0 && n > 15)
+if (n <= 0 || n > 15)
 {
 }
 else
@@ -19,7 +19,7 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 rst = (i * j);
-	if ( j != 0 && rst < 9)
+	if ( j != 0 && rst <= 9)
 	{
 	_putchar(',');
 	_putchar(' ');
