fff8ea68810980c4d66d8cfd02e4b017d450e915
Sgechuki <sgechuki@outlook.com>
2022-04-18 09:09:03 +0300

    Fix output bug

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,9 +10,9 @@ int main(void)
 {
 int i;
 
-long int sum = 0;
+int sum = 0;
 
-for (i = 1; i < 1024;)
+for (i = 1; i < 1024; i++) 
 {
 	if (i % 3 == 0 && i % 5 == 0)
 	{
@@ -27,6 +27,6 @@ for (i = 1; i < 1024;)
 	sum = sum + i;
 	}
 }
-printf("%ld\n", sum);
+printf("%d\n", sum);
 return (0);
 }
