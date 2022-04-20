874bedd818b8a89c071b8e3af844844aef2b4d89
Sgechuki <sgechuki@outlook.com>
2022-04-21 00:48:36 +0300

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: holds string
+ * Return: Always void
+ */
+void rev_string(char *s)
+{
+int cnt, i;
+
+for (cnt = 0; *(s + cnt) != '\0'; cnt++)
+{
+}
+for (i = cnt - 1, i >= 0; i--)
+{
+_putchar(*(s + i));
+}
+}
