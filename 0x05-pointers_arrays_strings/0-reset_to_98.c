3b14a794bdb511525d2ecd52c2c16e0ba0f38912
Sgechuki <sgechuki@outlook.com>
2022-04-20 19:13:36 +0300

    Comply with betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - updates the value it points to to 98
- * @*n: integer pointer *n as argument
+ * @ *n: integer pointer n as argument
  * returns - Always void
  */
 void reset_to_98(int *n)
