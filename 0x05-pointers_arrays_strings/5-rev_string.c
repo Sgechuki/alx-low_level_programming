7dfd72bc44a7212a7b16fe2434aca732833d325c
Sgechuki <sgechuki@outlook.com>
2022-04-23 07:24:27 +0300

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,12 +9,12 @@ void rev_string(char *s)
 {
 int cnt, i;
 
-for (cnt = 0; s[cnt]; cnt++)
+for (cnt = 0; *(s + cnt) != '\0'; cnt++)
 {
 }
-for (i = cnt - 1; s[i]; i--)
+for (i = (cnt - 1); i >= 0; i--)
 {
-_putchar(s[i]);
+_putchar(*(s + i));
 }
 _putchar('\n');
 }
