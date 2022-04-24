b68e8c767f6069e6f97055ebee71d250785c993d
Sgechuki <sgechuki@outlook.com>
2022-04-24 22:29:27 +0300

    Correct checker errors

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i, j;
+int j;
 
-for (i = 0; *(dest + i) != '\0'; i++)
-{
-}
 for (j = 0; j < n && src[j] != '\0'; j++)
 {
 dest[j] = src[j];
+}
+for ( ;j < n; j++)
+{
+dest[j] = '\0';
 }
 return (dest);
 }
