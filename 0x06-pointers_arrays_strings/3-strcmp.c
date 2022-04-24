32436be0cc71cdac2ebd7d0d88753f1c778cbecc
Sgechuki <sgechuki@outlook.com>
2022-04-24 10:22:53 +0300

    Correct checker

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,15 +3,15 @@
  * _strcmp - compares two strings
  * @s1: a string
  * @s2: a string
- * Return: 0 if equal, j if different
+ * Return: 0 if equal or j if it is different
  */
 
 int _strcmp(char *s1, char *s2)
 {
-int i; 
+int i = 0; 
 int j = 0;
 
-for (i = 0; *(s1 + i) != '\0' && j == 0;)
+while (*(s1 + i) != '\0' && j == 0)
 {
 j = s1[i] - s2[i];	
 i++;
