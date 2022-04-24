d86faa0c9beca4fca86b3f37aaa0068ee943b87c
Sgechuki <sgechuki@outlook.com>
2022-04-24 09:38:32 +0300

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * _strcmp - compares two strings
  * @s1: a string
  * @s2: a string
- * Return: Always return
+ * Return: Always return integer of their difference
  */
 
 int _strcmp(char *s1, char *s2)
