8c237f94f6597e6f0c900b96e96f1e6aa739d979
Sgechuki <sgechuki@outlook.com>
2022-04-25 22:29:35 +0300

    Fix checker errors

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
 /**
  * cap_string - capitalizes all words of a string
  * @s: string containing words to be capitalized
- * Retrun: a capitalized string
+ * Retrun: pointer to the capitalized string
  */
 char *cap_string(char *s)
 {
@@ -12,21 +12,22 @@ char c[] = {44, 59, 46, 63, 123, 125, 33, 32, 10, 9, 34, 40, 41};
 
 i = 0;
 j = 0;
+
 while (s[i] != '\0')
 {
-	if (i == 0 && (s[i] >= 97 && s[i] <= 122))
-	{
-		s[i] = s[i] - 32;
-	}
-	while (c[j] != '\0')
-	{
-	if (c[j] == s[i] && (s[i + 1] >= 97 && s[i + 1] <= 122))
-	{
-		s[i + 1] = s[i + 1] - 32;
-	}
-	j++;
-	}
-	i++;
+if (i == 0 && (s[i] >= 97 && s[i] <= 122))
+{
+s[i] = s[i] - 32;
 }
-	return (s);
+while (c[j] != '\0')
+{
+if (c[j] == s[i] && (s[i + 1] >= 97 && s[i + 1] <= 122))
+{
+s[i + 1] = s[i + 1] - 32;
+}
+j++;
+}
+i++;
+}
+return (s);
 }
