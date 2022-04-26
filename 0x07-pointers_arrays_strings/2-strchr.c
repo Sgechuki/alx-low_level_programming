55b12c776237cdbc4244fb43a86f41a6d3c360cd
Sgechuki <sgechuki@outlook.com>
2022-04-26 22:15:53 +0300

    Correct checker errors

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,17 @@
 char *_strchr(char *s, char c)
 {
 int i;
+char *p;
 
+p = '\0'
 i = 0;
 while (s[i] != '\0')
 {
 if (s[i] == c)
 {
-return (s + i);
+p = (s + i);
 }
 i++;
 }
-if (s[i] == '\0')
-{
-return ('\0');
-}
+return (p);
 }
