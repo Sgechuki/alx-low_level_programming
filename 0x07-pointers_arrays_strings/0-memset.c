9a005afd4b93fe65d46106a00aba91463575c4fc
Sgechuki <sgechuki@outlook.com>
2022-04-26 18:33:07 +0300

    Create memset replica

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: char array
+ * @b: char variable
+ * @n: number of bytes
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+i = 0;
+while (i < n && s[i] != '\0')
+{
+s[i] = b;
+}
+return (s);
+}
+
+
