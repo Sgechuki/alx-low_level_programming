a47d34f91c9f902d74fbcc927699a8cc1513fb14
Sgechuki <sgechuki@outlook.com>
2022-04-27 18:54:39 +0300

    Correct checker five

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+
  /**
  * *_strchr - locates a character in a string
  * @s: string to be searched
@@ -12,13 +12,17 @@ char *_strchr(char *s, char c)
 int i;
 
 i = 0;
-while (s[i])
+while (s[i] != '\0')
 {
-	if (s[i] == c)
-	{
-		return (s + i);
-	}
-	i++;
+if (s[i] == c)
+{
+return (s + i);
+}
+i++;
+}
+if (s[i] == c)
+{
+return (s + 1);
 }
 return ('\0');
 }
