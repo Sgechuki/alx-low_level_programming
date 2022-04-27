a466b0fa267498d5f37fea14a4e96166051d859f
Sgechuki <sgechuki@outlook.com>
2022-04-27 18:31:33 +0300

    Correct checker five

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string
- * @s: string array
- * @c: character variable
- * Return: pointer to first occurence of c or NULL
+ * *_strchr - locates a character in a string
+ * @s: string to be searched
+ * @c: character to find
+ * Return: pointer to first occurence of c in s or NULL
+ * if c not found in s
  */
 char *_strchr(char *s, char c)
 {
