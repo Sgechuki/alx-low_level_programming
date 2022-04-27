11c9f71be07d9ae8545e0ba6dcbe6f6dbc6537de
Sgechuki <sgechuki@outlook.com>
2022-04-27 18:14:45 +0300

    Remove tenp files

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,17 +12,14 @@ char *_strchr(char *s, char c)
 int i;
 
 i = 0;
-for (;s[i] != '\0'; i++)
+while (s[i])
 {
 	if (s[i] == c)
 	{
-		s = (s + i);
-		return (s)
+		return (s + i);
 	}
+	i++;
 }
-	if (s[i] == '\0')
-	{
-		return (NULL);
-	}
+return (NULL);
 }
 
