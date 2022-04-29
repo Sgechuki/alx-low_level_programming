d95dfd91c8c3a743338f27efe8b9d3d14b30a8a8
Sgechuki <sgechuki@outlook.com>
2022-04-29 11:55:22 +0300

    Fix output bug

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int _sqrt_recursion_1(int n, int i);
 
 /**
  * _sqrt_recursion - returns natural square root of a number
@@ -7,24 +8,35 @@
  */
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	i = 1;
-	if (n == 1)
-	{
-		return (1);
-	}
 	if (n <= 0)
 	{
 		return (-1);
 	}
-	i = i + 1;
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (_sqrt_recursion_1(n, 2))
+}
+/**
+ * _sqrt_recursion_1 - iterates to find square root of n
+ * @n: number to find natural square root
+ * @i: small number to start incrementing from
+ * Return: natural square root of n
+ */
+int _sqrt_recursion_1(int n, int i)
+{
+	if ((i * i) > n)
+	{
+		return (-1);
+	}
 	if ((i * i) == n)
 	{
 		return (i);
 	}
-	if ((i * i) != n)
+	else if
 	{
-		return (0 + _sqrt_recursion(n));
+		return (_sqrt_recursion_1(n, i + 1));
 	}
 }
+
