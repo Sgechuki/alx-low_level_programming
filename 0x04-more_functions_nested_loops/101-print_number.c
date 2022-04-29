c86d562fb5f0d736c07a7bbd17290f13bbd036bf
Sgechuki <sgechuki@outlook.com>
2022-04-29 22:14:10 +0300

    Fix checker errors

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,14 @@
  */
 void print_number(int n)
 {
-	int i;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		i = -1 * n;
-	}
-	else
-	{
-		i = n;
-	}
-	if (n / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar((i % 10) + '0');
+if (n < 0)
+{
+_putchar('-');
+n = -1 * n;
+}
+if (n / 10)
+{
+print_number(n / 10);
+}
+_putchar((n % 10) + '0');
 }
