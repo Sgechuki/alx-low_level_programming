f8400ef34440e4df2bdcc8f451aaad5e9662cddd
Sgechuki <sgechuki@outlook.com>
2022-04-29 23:55:15 +0300

    Fix compile error

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,35 +9,18 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, k, l, n;
+	int i, j;
 	int d1, d2;
 
 	d1 = 0;
 	d2 = 0;
+	j = (size - 1);
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				d1 = d1 + a[i][j];
-				break;
-			}
-		}
+		d1 = d1 + a[i][i];
+		d2 = d2 + a[i][j];
+		j--;
 	}
-	printf("%i, ", d1);
-	n = size;
-	for (k = 0; k < size; i++)
-	{
-		for (l = (size - 1); l >= 0; j--)
-		{
-			n = n - 1;
-			if (l == n)
-			{
-				d2 = d2 + a[k][l];
-				break;
-			}
-		}
-	}
-	printf("%i\n", d2);
 }
+
+
