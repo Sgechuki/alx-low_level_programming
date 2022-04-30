1e8fa58c69244086a134ed77bc56ce850d6135a6
Sgechuki <sgechuki@outlook.com>
2022-04-30 19:36:27 +0300

    Comply betty style

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,15 +9,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1, sum2;
+int i, j, sum1, sum2;
 
-	sum1 = 0;
-	sum2 = 0;
-	j = (size - 1);
-	for (i = 0; i < size; i++)
-	{
-		sum1 += a[(size * i) + i];
-		sum2 += a[(j * i) + j];
-	}
-	printf("%d, %d\n", sum1, sum2);
+sum1 = 0;
+sum2 = 0;
+j = size - 1;
+for (i = 0; i < size; i++)
+{
+sum1 = sum1 + a[size * i + i];
+sum2 = sum2 + a[j * i + j];
+}
+printf("%d, %d\n", sum1, sum2);
 }
