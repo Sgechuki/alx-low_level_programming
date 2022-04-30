72fb2ed875f968d4e31c5d2112cd62194b9d55ff
Sgechuki <sgechuki@outlook.com>
2022-04-30 10:15:22 +0300

    Remove temp files

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,18 +9,19 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	int d1, d2;
+int i, j;
+int sum1, sum2;
 
-	d1 = 0;
-	d2 = 0;
-	j = size;
-	for (i = 0; i < size; i++)
-	{
-		d1 = d1 + a[i][i];
-		d2 = d2 + a[i][j - 1];
-		j--;
-	}
+d1 = 0;
+d2 = 0;
+j = size - 1;
+for (i = 0; i < size; i++)
+{
+sum1 += a[i][i];
+sum2 += a[i][j];
+j--;
+}
+printf("%d, %d\n", sum1, sum2);
 }
 
 
