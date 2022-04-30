dd0619530c22446387f6c71f22236799686e266f
Sgechuki <sgechuki@outlook.com>
2022-04-30 22:00:02 +0300

    Comply betty style

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,7 +5,7 @@
  * print_diagsums - print sum of each diagonals
  * @a: square matrix to sum diagonals
  * @size: size of square matrix
- * 
+ *
  */
 void print_diagsums(int *a, int size)
 {
@@ -16,8 +16,8 @@ sum2 = 0;
 j = size - 1;
 for (i = 0; i < size; i++)
 {
-sum1 = sum1 + a[size * i + i];
-sum2 = sum2 + a[j * i + j];
+sum1 = sum1 + a[size *i + i];
+sum2 = sum2 + a[j *i + j];
 }
 printf("%d, %d\n", sum1, sum2);
 }
