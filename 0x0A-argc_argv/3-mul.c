2e816c577ebd3afd9c25e9bd1dbbfb5fe123e0d7
Sgechuki <sgechuki@outlook.com>
2022-05-03 13:58:55 -0700

    Correct checker errors

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
  * main - multiply two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 (Succes)
+ * Return: 0 (Success) or 1 error
  */
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
 
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
