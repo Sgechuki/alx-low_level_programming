f0f0e621443b938c4b3ad2dc596126bf5eb055ac
Sgechuki <sgechuki@outlook.com>
2022-05-10 14:32:43 -0700

    Correct pointer error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *arr;
+	unsigned int *arr;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (0);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		arr[i] = 0;
 	}
