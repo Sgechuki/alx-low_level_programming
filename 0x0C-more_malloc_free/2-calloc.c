ff8af74874f356f973328a46145fbe16077f2dc7
Sgechuki <sgechuki@outlook.com>
2022-05-10 15:37:56 -0700

    Correct checker errors

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,21 +10,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *arr;
-	unsigned int i;
+unsigned int *ptr;
+unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (0);
 	}
-	arr = malloc(size * nmemb);
+	ptr = malloc(size * nmemb);
 	if (arr == NULL)
 	{
 		return (0);
 	}
 	for (i = 0; i < (nmemb * size); i++)
 	{
-		arr[i] = 0;
+		ptr[i] = 0;
 	}
-	return (arr);
+	return (ptr);
 }
