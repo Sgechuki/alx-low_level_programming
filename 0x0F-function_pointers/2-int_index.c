87886fb0000c1e180722cba31689c9f8afbba180
Sgechuki <sgechuki@outlook.com>
2022-05-16 13:05:23 -0700

    Fix compile error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,21 +6,21 @@
  * @array: pointer to integer array
  * @size: number of elements in array
  * @cmp: function pointer
- * Return: -1 if no match or i on first match
+ * Return: -1 if no match or "i" on first match
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array && size && cmp)
-	{
-		int i;
+	int i;
 
-		if (size <= 0)
-		{
-			return (-1);
-		}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array && cmp)
+	{
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]))
+			if (cmp(array[i]) != 0)
 			{
 				return (i);
 			}
