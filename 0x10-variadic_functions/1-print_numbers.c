a9b14ae00d08d4db1811e801db23a58d0e9c4d89
Sgechuki <sgechuki@outlook.com>
2022-05-17 09:42:01 -0700

    Correct check code error 10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,20 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int i;
 	va_list ap;
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	va_start(ap, n);
 	for (i = 0; i < (int) n; i++)
 	{
+		if (separator == NULL)
+		{
+			printf("%d", va_arg(ap, int));
+		}
+		else
+		{
 		printf("%d", va_arg(ap, int));
 		if (i < (int) n - 1)
 		{
 			printf("%s", separator);
+		}
 		}
 	}
 	va_end(ap);
