c33750eee2fad4e503e821a1e9fd1902220df4bf
Sgechuki <sgechuki@outlook.com>
2022-05-25 09:30:29 -0700

    Print text before main execution

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * before_main - prints text before main function is executed
+ *
+ *
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
