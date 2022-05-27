a235badfd7abeed31005fb2d2cd5386d655089b7
Sgechuki <sgechuki@outlook.com>
2022-05-27 14:36:17 -0700

    Sleep now

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
-	if (head == NULL || new_node == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (*head != NULL)
+	while ((*head)->next != NULL)
 	{
 		if (idx > 0 && i == (idx - 1))
 		{
