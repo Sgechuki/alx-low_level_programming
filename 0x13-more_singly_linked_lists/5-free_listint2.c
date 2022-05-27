31405b5dc2feec6524184b4176d7f7fb3e658b2c
Sgechuki <sgechuki@outlook.com>
2022-05-27 12:02:05 -0700

    Fix checher error 9

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,10 +11,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
+	if (head == NULL)
+	{
+		return;
+	}
 	while (*head != NULL)
 	{
-		temp = temp->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
