ac5833f02bcfbdcf355d79a53c5a78b55510d9c7
Sgechuki <sgechuki@outlook.com>
2022-05-27 12:25:41 -0700

    Fix output bug

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,12 +10,15 @@
 int pop_listint(listint_t **head)
 {
 	int i;
+	listint_t *temp;
 
 	if (head == NULL)
 	{
 		return (0);
 	}
+	temp = (*head)->next;
 	i = (int) (*head)->n;
 	free(*head);
+	*head = temp;
 	return (i);
 }
