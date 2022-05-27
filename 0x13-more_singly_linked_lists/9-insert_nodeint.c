e01aa4dd01b83a500bc7aaf5682a3a6641f683f8
Sgechuki <sgechuki@outlook.com>
2022-05-27 14:17:51 -0700

    One last chance

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,10 +24,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	i = 0;
 	while (*head != NULL)
 	{
-		if (i == (idx - 1))
+		if (idx > 0 && i == (idx -1))
 		{
 			new_node->next = (*head)->next;
 			(*head)->next = new_node;
+			return (new_node);
+		}
+		else if (idx == 0)
+		{
+			new_node->next = *head;
+			*head = new_node;
 			return (new_node);
 		}
 		*head = (*head)->next;
