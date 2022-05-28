bb26ecaadd07222d2a42179c28a2a4efbe222908
Sgechuki <sgechuki@outlook.com>
2022-05-27 22:57:40 -0700

    Wake up and bug

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,5 +39,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = (*head)->next;
 		i++;
 	}
+	if (*head == NULL && i == idx)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 	return (NULL);
 }
