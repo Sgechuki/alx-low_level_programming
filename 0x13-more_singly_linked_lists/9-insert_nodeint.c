d67aaa60cd6aa0781d02f3ecd1ddff0ccdf6bcbe
Sgechuki <sgechuki@outlook.com>
2022-05-27 23:47:41 -0700

    Oh wow now

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,15 +36,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	for (i = 0; i < idx - 1 && *head; i++)
+	i = 0;
+	while (*head != NULL)
 	{
+		if (i == idx -1)
+		{
+			new_node->next = (*head)->next;
+			(*head)->next = new_node;
+			return (new_node);
+		}
 		*head = (*head)->next;
+		i++;
 	}
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
-	new_node->next = (*head)->next;
-	(*head)->next = new_node;
-	return (new_node);
+	return (NULL);
 }
