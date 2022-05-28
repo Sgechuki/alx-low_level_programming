4bd4a41b49915553ebbaff5b9cc273a920996567
Sgechuki <sgechuki@outlook.com>
2022-05-27 23:53:03 -0700

    Heh

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -48,5 +48,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = (*head)->next;
 		i++;
 	}
-	return (NULL);
+	(*head)->next = new_node;
+	return (new_node);
 }
