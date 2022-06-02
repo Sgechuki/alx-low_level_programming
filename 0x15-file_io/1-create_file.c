93ebb1df9ea42920df56af954b488378628f75ef
Sgechuki <sgechuki@outlook.com>
2022-06-02 13:42:38 -0700

    Fix checker error 8

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,13 +7,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, fwrite, i;
+	int fd, n, i;
 
 	if (!filename)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
@@ -23,8 +23,8 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i]; i++)
 		{
 		}
-		fwrite = write(fd, text_content, i);
-		if (fwrite == -1)
+		n = write(fd, text_content, i);
+		if (n == -1)
 		{
 			return (-1);
 		}
