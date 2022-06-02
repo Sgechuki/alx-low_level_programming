0d4e052d6e97aad5d38cf0b04e9b7695845220d0
Sgechuki <sgechuki@outlook.com>
2022-06-02 13:31:05 -0700

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content)
 	{
-		fwrite = write(fd, text_content, sizeof(text_content));
+		fwrite = write(fd, text_content, strlen(text_content));
 		if (fwrite == -1)
 		{
 			return (-1);
