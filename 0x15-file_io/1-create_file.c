b351cdb9e3096727ede7c4b543e8ba4b7cbad3dd
Sgechuki <sgechuki@outlook.com>
2022-06-02 13:26:43 -0700

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fwrite;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		fwrite = write(fd, text_content, sizeof(text_content));
+		if (fwrite == -1)
+		{
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
