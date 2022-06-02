16b268a4c778e37cf78fabf3354f5562bd2cee42
Sgechuki <sgechuki@outlook.com>
2022-06-02 12:34:57 -0700

    Read a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to string with name of file
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t i, n;
+	char *buf;
+
+	if (!filename)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	i = read(fd, buf, letters);
+	if (i == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	n = write(STDIN_FILENO, buf, i);
+	if (n < i)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (i);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This directory holds my solution submission for "Project: 0x15. C-File I/O" tasks
