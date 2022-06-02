4c9c8e702feb68803ab407f8a4d6f7b6af3a61d8
Sgechuki <sgechuki@outlook.com>
2022-06-02 13:38:54 -0700

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,16 +13,16 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	for (i = 0; text_content[i]; i++)
+	if (text_content != NULL)
 	{
-	}
-	if (text_content)
-	{
+		for (i = 0; text_content[i]; i++)
+		{
+		}
 		fwrite = write(fd, text_content, i);
 		if (fwrite == -1)
 		{
