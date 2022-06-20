ce13389c0e138af22646dbb1a1506695c6caca68
Sgechuki <sgechuki@outlook.com>
2022-06-20 13:21:07 -0700

    Handle int min

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num = 0, skim = 0;
+	int i = 0, sign = -1, num = 0, skim = 0;
 
 	for (i = 0; s[i]; i++)
 	{
@@ -18,7 +18,7 @@ int _atoi(char *s)
 		}
 		while ((s[i] >= '0' && s[i] <= '9') && s[i])
 		{
-			num = (num * 10) + (s[i] - '0');
+			num = (num * 10) - (s[i] - '0');
 			skim = 1;
 			i++;
 		}
