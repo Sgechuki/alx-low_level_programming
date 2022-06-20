3a903c5ce66a695f677e8fbade55d704e8d1aea6
Sgechuki <sgechuki@outlook.com>
2022-06-20 11:01:57 -0700

    Code adjustment

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num = 0, plv = 0;
+	int i = 0, sign = 1, num = 0, skim = 0;
 
 	for (i = 0; s[i]; i++)
 	{
@@ -18,18 +18,11 @@ int _atoi(char *s)
 		}
 		while ((s[i] >= '0' && s[i] <= '9') && s[i])
 		{
-			if (plv == 0)
-			{
-				num = s[i] - '0';
-				plv = 1;
-			}
-			else if (plv)
-			{
-				num = (num * 10) + (s[i] - '0');
-			}
+			num = (num * 10) + (s[i] - '0');
+			skim = 1;
 			i++;
 		}
-		if (plv == 1)
+		if (!skim)
 		{
 			num *= sign;
 			break;
