8f4e294ebfc4ccfe0bcf2f2c9cb965d3e60d4342
Sgechuki <sgechuki@outlook.com>
2022-06-25 00:16:11 -0700

    Fix output error

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,24 +7,25 @@
  */
 void print_number(int n)
 {
-	int pv = 1, copy1 = n, copy2 = n;
+	int pv = 1, copy1 = n, copy2 = n, ones = n % 10;
 
-	if (n < 0)
+	if (ones < 0)
 	{
-		copy1 = -1 * copy1;
-		copy2 = -1 * copy2;
+		copy1 *= -1;
+		copy2 *= -1;
+		ones *= -1;
 		_putchar('-');
 	}
 	while (copy1 / 10)
 	{
 		pv *= 10;
-		copy1 = copy1 / 10;
+		copy1 /= 10;
 	}
-	while (pv >= 10)
+	while (pv > 1)
 	{
 		_putchar((copy2 / pv) + '0');
 		copy2 -= (copy2 / pv) * pv;
 		pv = pv / 10;
 	}
-	_putchar(copy2 + '0');
+	_putchar(ones + '0');
 }
