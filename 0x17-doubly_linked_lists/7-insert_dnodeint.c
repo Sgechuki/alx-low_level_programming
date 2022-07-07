05ba8dd2acd0a5ea682e578c9851dee6892de388
Sgechuki <sgechuki@outlook.com>
2022-07-07 12:03:01 -0700

    Comply betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		return (add_dnodeint(h,  n));
-	}	
+	}
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 	{
