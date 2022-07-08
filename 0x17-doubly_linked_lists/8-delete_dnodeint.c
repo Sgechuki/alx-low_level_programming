6d3a3722572543e560ed9bf558e1c77b68af23de
Sgechuki <sgechuki@outlook.com>
2022-07-08 11:30:17 -0700

    Weuh

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,7 +9,7 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp1, *temp2;
+	dlistint_t *temp1;
 	unsigned int i = 0;
 
 	if (!*head)
@@ -17,31 +17,30 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	}
 	temp1 = *head;
-	if (temp1 && index == 0)
+	if (index == 0)
 	{
-		*head = (*head)->next;
-		(*head)->prev = NULL;
+		*head = temp1->next;
+		if (temp1->next != NULL)
+		{
+			temp1->next->prev = NULL;
+		}
 		free(temp1);
 		return (1);
 	}
-	while (temp1 && i < index)
+	while (i < index)
 	{
-		temp2 = temp1;
+		if (temp1->next == NULL)
+		{
+			return (-1);
+		}
 		temp1 = temp1->next;
 		i++;
 	}
-	if (temp2->next && i== index)
+	temp1->prev->next = temp1->next;
+	if (temp1->next != NULL)
 	{
-		temp2->next = temp1->next;
-		temp1->next->prev = temp2;
-		free(temp1);
-		return (1);
+		temp1->next->prev = temp1->prev;
 	}
-	if (!temp2->next && i == index)
-	{
-		temp2->prev->next = NULL;
-		free(temp2);
-		return(1);
-	}
+	free(temp1);
 	return (-1);
 }
