1db57f3e3c061bd626f6586fe4d88ab0047453c6
Sgechuki <sgechuki@outlook.com>
2023-06-01 01:47:47 -0700

    Capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string  - capitalizes all  words of a string
+ * @s: string to be capitalized
+ * Return: pointer to capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	char delim [] = {44, 59, 46, 63, 123, 125, 33, 32, 10, 9, 34, 40, 41};
+	int i, j;
+
+	if (s[0] >= 97 && s[0] <= 122)
+	{
+		s[0] = s[0] - 32;
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; delim != '\0'; j++)
+		{
+			if (s[i] == delim[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
